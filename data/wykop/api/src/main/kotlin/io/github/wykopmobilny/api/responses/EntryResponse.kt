70f2bb574006984263e8cfe191f830f6e174133f
Kiciuk <kiiciook@gmail.com>
2023-02-08 17:41:18 +0100

    [API V3][RESPONSE][ENTRY] Update definition

--- data/wykop/api/src/main/kotlin/io/github/wykopmobilny/api/responses/EntryResponse.kt
+++ data/wykop/api/src/main/kotlin/io/github/wykopmobilny/api/responses/EntryResponse.kt
@@ -7,19 +7,25 @@ import kotlinx.datetime.Instant
 @JsonClass(generateAdapter = true)
 data class EntryResponse(
     @field:Json(name = "id") val id: Long,
-    @field:Json(name = "date") val date: Instant,
-    @field:Json(name = "body") val body: String?,
+    @field:Json(name = "slug") val slug: String,
     @field:Json(name = "author") val author: AuthorResponse,
-    @field:Json(name = "blocked") val blocked: Boolean,
+    @field:Json(name = "device") val device: String,
+    @field:Json(name = "created_at") val created_at: Instant,
+    @field:Json(name = "content") val content: String?,
+    @field:Json(name = "media") val media: Boolean, //mediaresponse
+    @field:Json(name = "adult") val adult: Boolean
+    @field:Json(name = "tags") val tags: Boolean //array of string
     @field:Json(name = "favorite") val favorite: Boolean,
-    @field:Json(name = "vote_count") val voteCount: Int,
-    @field:Json(name = "comments_count") val commentsCount: Int,
-    @field:Json(name = "comments") val comments: List<EntryCommentResponse>?,
+    @field:Json(name = "parent") val parent: Long, //shouldnt that be in comment_response?
+    @field:Json(name = "votes") val votes: Int, //object UP:INT,DOWN,INT,USERS:ARRAY?
+    @field:Json(name = "editable") val editable: Boolean,
+    @field:Json(name = "deletable") val deletable: Boolean,
+   
+    @field:Json(name = "comments") val comments: List<EntryCommentResponse>?, //object items:array, count:int
+    @field:Json(name = "resource") val resource: String,
+    
+    @field:Json(name = "actions") val actions: String, //object create,update,delete,vote_up,create_favourite,report:BOOL
+    @field:Json(name = "archive") val archive: Boolean,
     @field:Json(name = "status") val status: String,
-    @field:Json(name = "embed") val embed: EmbedResponse?,
-    @field:Json(name = "survey") val survey: SurveyResponse?,
-    @field:Json(name = "user_vote") val userVote: Int,
-    @field:Json(name = "violation_url") val violationUrl: String?,
-    @field:Json(name = "app") val app: String?,
-    @field:Json(name = "can_comment") val isCommentingPossible: Boolean?,
+   
 )
